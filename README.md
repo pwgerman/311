dd1a5f3e706ed761264636772036c83f412fb9b6
Mark Everett <mark@mynnx.com>
2015-10-02 16:33:59 -0700

    Initial commit of webapp Static files came from Initializr: http://www.initializr.com/

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 311
+
+Installation Instructions
+=========================
+
+Ideally inside a [virtualenv](http://docs.python-guide.org/en/latest/dev/virtualenvs/):
+
+```
+pip install -r requirements.txt
+python web/app.py
+```
+
---
+++ web/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('index.html');
+
+@app.route('/ticket/<ticket>')
+def get_ticket(ticket):
+    return {
+        'im a': 'ticket'
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
