281edb585d90b4be00294f17496c6ac2cb3d208f
walter <pwgerman@gmail.com>
2015-10-07 14:23:57 -0700

    ml311.predict converts string to datetime

--- ml311.py
+++ ml311.py
@@ -1,13 +1,16 @@
 # Python 2.7 module for machine learning predictions for San Francisco 311
 
-def predict(opendate):
-    """Form a prediction.
+from datetime import datetime
+from pandas import to_datetime
+
+def predict(opendate, sup_dist=0):
+    """Form a close date prediction as datetime object.
 
     Keyword arguments:
     opendate -- The case open date. {string or datetime}.
     sup_dist -- The case supervisor district. {int} (default 0 for none)
     """
-    from datetime import datetime
+    opendate = to_datetime(opendate)
     return datetime.now()
 
 def helloworld():
