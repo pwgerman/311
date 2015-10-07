693d9b131d55e80775da65439128e6a60fce08d6
walter <pwgerman@gmail.com>
2015-10-07 13:44:26 -0700

    ml311 predict outputs datetime for today

--- ml311.py
+++ ml311.py
@@ -1,7 +1,14 @@
 # Python 2.7 module for machine learning predictions for San Francisco 311
 
 def predict(opendate):
-	pass
+    """Form a prediction.
+
+    Keyword arguments:
+    opendate -- The case open date. {string or datetime}.
+    sup_dist -- The case supervisor district. {int} (default 0 for none)
+    """
+    from datetime import datetime
+    return datetime.now()
 
 def helloworld():
     print 'Hello world'
