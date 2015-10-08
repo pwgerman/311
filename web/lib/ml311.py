a8057b0f3e5d3178ffdb6f14b1af897d21ada644
walter <pwgerman@gmail.com>
2015-10-08 10:22:29 -0700

    updates for predict to return timedeltas

--- web/lib/ml311.py
+++ web/lib/ml311.py
@@ -1,16 +1,16 @@
 # Python 2.7 module for machine learning predictions for San Francisco 311
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from pandas import Series, to_datetime
 from time import sleep
 
-def delta2secs(timedelta):
+def delta2sec(timedelta):
     """convert timedelta to seconds for calculations"""
     return Series(timedelta).astype('timedelta64[s]')
 
-def secs2delta(time_secs):
+def sec2delta(time_sec):
     """Convert time in seconds to timedelta"""
-    return timedelta(0, time_secs)
+    return timedelta(0, time_sec[0])
                 
 def predict(opendate, sup_dist=0):
     """Form a close date prediction as datetime object.
@@ -20,6 +20,8 @@ def predict(opendate, sup_dist=0):
     sup_dist -- The case supervisor district. {int} (default 0 for none)
     """
     opendate = to_datetime(opendate)
-    return datetime.now()
+    today = datetime.now()
+    duration = delta2sec(today-opendate)
+    return sec2delta(duration)
 
 
