5013c3d21ce1589a37550bc7d6b5ff616b466f3e
walter <pwgerman@gmail.com>
2015-10-16 04:49:21 -0700

    Datetime munging functions

--- web/lib/ml311.py
+++ web/lib/ml311.py
@@ -2,7 +2,83 @@
 
 from datetime import datetime, timedelta
 from pandas import Series, to_datetime, Timedelta
+import pandas as pd
 from time import sleep
+
+dist_names = ['None', 'Richmond','Marina', 'North Beach to Market', 'Sunset', \
+            'Panhandle', 'Soma to Tenderloin', 'West Portal to Merced', \
+            'Noe Valley', 'Mission to Bernal', 'Bay View', 'Excelsior']
+
+def load_data(filename):
+    """load a saved datafile with datetime conversion for times
+    RETURNS a pandas DataFrame of the csv file from 311.
+    Designed for either raw file from 311 or processed file with datetime
+    converted."""
+    ## Loading a CSV file, without a header (so we have to provide field names)
+    df = pd.read_csv(filename, index_col=0)  
+
+    # if 'Opened_dt in columns'
+    if 'Opened_dt' in df.columns:
+        df['Opened_dt']=pd.to_datetime(df['Opened_dt'])
+    if 'Closed_dt' in df.columns:
+        df['Closed_dt']=pd.to_datetime(df['Closed_dt'])
+
+    return df
+    
+def save_data(df, newfilename):
+    """This will save datafiles in a format for datetimes to be read in more
+    quickly."""
+    # if 'Opened_dt' in df.columns ... convert to str
+   # if 'Opened_dt in columns'
+    if 'Opened_dt' in df.columns:
+        df['Opened_dt']= df['Opened_dt'].astype('str')
+    if 'Closed_dt' in df.columns:
+        df['Closed_dt']= df['Closed_dt'].astype('str')
+
+    # then save
+    df.to_csv(path_or_buf=newfilename)
+    return newfilename
+
+def process_timedelta(df):
+    """Takes a DateFrame with datetimes already coonverted for times
+    RETURNS a pandas DataFrame with timedeltas for case total open time."""
+
+    df['Case_Time'] = df['Closed_dt']-df['Opened_dt']
+    df['Case_Sec'] = delta2sec(df['Case_Time'])
+
+    return df
+
+# change name to process_datetimes and remove CaseTime and Case_Sec
+def process_datetime(df):     
+    """Loads a raw dataset, reformats some rows for faster processing and saves 
+    to a new file for efficiently reading in later with load_data().
+    Creates the 'Opened_dt' and 'Closed_dt' columns.
+    Move save functionality to another function."""
+
+    # generate new columns with datetime format for efficient calculations
+    # and total case time open in seconds
+    if 'Opened_dt' in df.columns:
+        df['Opened_dt']= pd.to_datetime(df['Opened_dt'])
+    else:
+        print 'Datetime efficient string not found. Processing: wait 10 min.'
+        df['Opened_dt']= pd.to_datetime(df['Opened'])
+
+    if 'Closed_dt' in df.columns:
+        df['Closed_dt']= pd.to_datetime(df['Closed_dt'])
+    else:
+        print 'Datetime efficient string not found. Processing: wait 10 min.'
+        df['Closed_dt']= pd.to_datetime(df['Closed'])
+    # df['Case_Time'] = df['Closed_dt']-df['Opened_dt']
+    # df['Case_Sec'] = delta2sec(df['Case_Time'])
+
+    # # format for saving datetimes
+    # df['Opened_dt']= df['Opened_dt'].astype('str')
+    # df['Closed_dt']= df['Closed_dt'].astype('str')
+    # df['Case_Time']= df['Case_Time'].astype('str')
+
+    # df.to_csv(path_or_buf=newfilename)
+    return df
+
 
 def delta2sec(timedelta):
     """convert timedelta to seconds for calculations"""
