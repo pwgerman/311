1a20d025f060aef9e523bf3391c4c81a1c68036a
walter <pwgerman@gmail.com>
2015-11-23 16:47:12 -0800

    Use urllib2

--- web/lib/download311.py
+++ web/lib/download311.py
@@ -1,5 +1,8 @@
 # python 2.7
 
+
+import urllib2
+
 def download_csv(url='https://data.sfgov.org/api/views/vw6y-z8j6/rows.csv', savefilename):
     """Downsloads csv file of 311 case data from SF server.  Arguments are
     URL of file and path/filename for locally saving the file."""
