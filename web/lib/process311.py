f6758f72985aaebe12b65931601764ec4e603f29
walter <pwgerman@gmail.com>
2015-11-24 08:15:55 -0800

    load_new_data()

--- web/lib/process311.py
+++ web/lib/process311.py
@@ -2,14 +2,29 @@
 
 import ml311
 
-rawdata_filename = '/Users/walter/Data/SF/Case_Data_from_San_Francisco_311__SF311_2015-10-22.csv'
-# procdata_filename = rawdata_filename.strip('.cvs')+'proc.csv'
 
 def make_proc_filename(rawdata_filename):
     """Returns the filename for processed datafile."""
     procdata_filename = rawdata_filename.strip('.cvs')+'proc.csv'
     return procdata_filename
 
+def load_new_data(rawdata_filename, procdata_filename):
+    """Takes a raw SF 311 csv datafile, processes the dates into datetime
+    format and then saves the output for faster loading in the future.
+    Also, returns the processed pandas DataFrame."""
+    print 'This may take a few minutes to load'
+    t0 = time.time()
+    df = ml311.load_data(rawdata_filename)
+    print str(time.time()-t0) + ' seconds elapsed'
+    sys.stdout.flush()    # print before executing next statement
+    t0 = time.time()
+    df = ml311.process_datetime(df)
+    ml311.save_data(df, procdata_filename)
+    print str(time.time()-t0) + ' seconds elapsed'
+    return df
+
 
 if __name__ == "__main__":
+	rawdata_filename = '/Users/walter/Data/SF/Case_Data_from_San_Francisco_311__SF311_2015-10-22.csv'
 	procdata_filename = make_proc_filename(rawdata_filename)
+
