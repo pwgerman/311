2598df361bc9bd13b6469c81159c65e39f4396c3
walter <pwgerman@gmail.com>
2015-11-23 17:22:27 -0800

    Remove downlod_url() which is replaced by download()

--- web/lib/download311.py
+++ web/lib/download311.py
@@ -13,40 +13,6 @@ def download_csv(url='https://data.sfgov.org/api/views/vw6y-z8j6/rows.csv', save
     URL of file and path/filename for locally saving the file."""
     urllib.urlretrieve(url, "urllib_test.csv")
 
-
-# https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
-def download_url(url='https://data.sfgov.org/api/views/vw6y-z8j6/rows.csv', as_filename=None):
-    # url = "https://i.imgur.com/G0IKECz.jpg"
-    # url='https://data.sfgov.org/api/views/vw6y-z8j6/rows.csv'
-
-    file_name = url.split('/')[-1]
-    u = urllib2.urlopen(url)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    pdb.set_trace()
-    # meta.headers   # will list headers available
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print status,
-
-    f.close()
-
-# download_url()
-# download_url(url="https://i.imgur.com/G0IKECz.jpg")
-
-
 # https://stackoverflow.com/questions/862173/how-to-download-a-file-using-python-in-a-smarter-way
 def append2filename(filename, add=None):
     """Appends extra string to end of filename, before the first 'dot' extension.
